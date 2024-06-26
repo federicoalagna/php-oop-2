9ace67e478aa46c76f66da6b8893599cc505cdbb
federicoalagna <federicoalagna01@gmail.com>
2024-06-26 17:34:40 +0200

    Creazione delle istanze, stampa delle card in pagina e aggiunta del CSS

--- index.php
+++ index.php
@@ -4,9 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Shop Animali</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
+
     <?php
           
         // Classe che rappresenta le due categorie di animali (cani e gatti)
@@ -30,7 +33,7 @@
         }
 
         // Tipo di prodotto
-        
+
         abstract class ItemType {
             protected $typeName;
 
@@ -127,7 +130,62 @@
                 }
             }
 
+                       
+            $dogCategory = new Category("Cani", "fas fa-dog");
+            $catCategory = new Category("Gatti", "fas fa-cat");
+
+       
+            // var_dump($dogCategory);
+            // var_dump($catCategory);
+
+         
+            $dogFood = new FoodProduct("Croccantini", 29.99, "https://iperverde.it/cdn/shop/products/nutrimi-all-breeds-cibo-per-cane-adulto-400-gr-maiale.jpg?v=1605292236", $dogCategory);
+            $catToy = new ToyProduct("Palla da gioco", 9.99, "https://arcaplanet.vtexassets.com/arquivos/ids/283729/palla-snack-gioco-per-gatti-verde.jpg?v=638168828051000000", $catCategory);
+            $dogKennel = new KennelProduct("Cuccia", 59.99, "https://www.casadellozerbino.com/1041-superlarge_default/cuccia-per-cani-domus-in-plastica-da-esterno-ricovero-per-cani-trattato-con-impregnanti-ecologici.jpg", $dogCategory);
+
+         
+            // var_dump($dogFood);
+            // var_dump($catToy);
+            // var_dump($dogKennel);
+
+
+// Funzione per stampare la card del prodotto
+function printProductCard(Product $product) {
+    $category = $product->getCategory();
+    $itemType = $product->getItemType();
+
+
+    echo "<div class='col-md-4'>
+            <div class='card product-card'>
+                <img class='card-img-top' src='" . $product->getImagePath() . "' alt='" . $product->getTitle() . "'>
+                <div class='card-body'>
+                    <h5 class='card-title'>" . $product->getTitle() . "</h5>
+                    <p class='card-text'>
+                        <i class='" . $category->getIcon() . " category-icon'></i> 
+                        " . $category->getName() . " - " . $itemType->getTypeName() . "
+                    </p>
+                    <p class='card-text'>Prezzo: $" . $product->getPrice() . "</p>
+                </div>
+            </div>
+          </div>";
+
+          
+}
     ?>
+        <div class="container mt-5">
+        <div class="row">
+            <?php
+            printProductCard($dogFood);
+            printProductCard($catToy);
+            printProductCard($dogKennel);
+            ?>
+        </div>
+        <div class="row">
+            <div class="col-12 text-center">
+                <p>Numero totale di prodotti: <?php echo Product::getProductCount(); ?></p>
+            </div>
+        </div>
+    </div>
 
 </body>
 </html>
